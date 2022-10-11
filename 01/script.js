2dd3248e950d53456d75bc0209e597c67c55de8d
vita-klejch <vita9@post.cz>
2022-10-11 21:11:26 +0200

    01 - Java Script - basic drill

--- 01/script.js
+++ 01/script.js
@@ -1,7 +1,7 @@
 // Sample Output : Today is : Tuesday.
 // Current time is : 10 PM : 30 : 38
 root = document.getElementById("roort");
-function show_time() {
+function get_time() {
   today = new Date();
   //   console.log(today);
   day_names = [
@@ -21,7 +21,44 @@ function show_time() {
     hour -= 12;
     prefix = "PM";
   }
-  console.log(
-    `Aktualni cas je : ${hour} ${prefix} : ${today.getMinutes()} : ${today.getSeconds()}`
-  );
+  answer = `Aktualni cas je : ${hour} ${prefix} : ${today.getMinutes()} : ${today.getSeconds()}`;
+  return answer;
+}
+root = document.getElementById("root");
+function show_it() {
+  time = get_time();
+  root.innerHTML = time;
+}
+setInterval(show_it, 1000);
+
+const root2 = document.getElementById("root2");
+text = "x";
+function add_letter() {
+  text = text + "x";
+  root2.innerText = text;
+}
+setInterval(add_letter, 2000);
+
+// Formula : c/5 = (f-32)/9 [ where c = temperature in Celsius and f = temperature in Fahrenheit ]
+// Expected Output :
+// 60°C is 140 °F
+// 45°F is 7.222222222222222°C
+function count_celsius() {
+  display_celsius = document.getElementById("display_celsius");
+  display_fahrenheit = document.getElementById("display_fahrenheit");
+  number = document.getElementById("number_input").value;
+  result = number;
+  celsius = ((number - 32) / 9) * 5;
+  fahrenheit = (number / 5) * 9 + 32;
+  display_celsius.innerText = `${number} stupnu Fahrenheit je ${celsius} stupnu Celsia`;
+  display_fahrenheit.innerText = `${number} stupnu Celsia je ${fahrenheit} stupnu Fahrenheit`;
+}
+
+// if input starts with "Py", then nothing happens, else Py is added
+function change_text() {
+  text_input = document.getElementById("text_input").value;
+  text_output = document.getElementById("text_output");
+  text_input = text_input.startsWith("Py") ? text_input : "Py" + text_input;
+
+  text_output.innerText = text_input;
 }
