b6dc22f3af6331418846397ee9fb0f0ec83addcc
vita-klejch <vita9@post.cz>
2023-06-01 23:39:09 +0200

    some files added by mistake - files are deleted

--- ProjectJS_rekvalifikace-main/js/injured.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-class Pojistenec {
-    constructor(celeJmeno, telefon, vek) {
-        this.celeJmeno = celeJmeno;
-        this.telefon = telefon;
-        this.vek = vek;
-    }
-}
--- ProjectJS_rekvalifikace-main/js/service.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-const listOfInjured = new ListOfInjured();
-listOfInjured.otevritSkrytFormular();
-listOfInjured.zapisParametruPojistence();
-listOfInjured.seznamPriOtevreni();
-listOfInjured.smazatPojistence();
